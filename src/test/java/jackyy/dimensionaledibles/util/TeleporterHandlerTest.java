da7acdce9420bf6ad8a6587f5efea99c370a6def
Exa <11907282+Exaxxion@users.noreply.github.com>
2021-01-28 23:34:00 -0800

    Add a test case for making sure players don't spawn in lava sources

--- src/test/java/jackyy/dimensionaledibles/util/TeleporterHandlerTest.java
+++ src/test/java/jackyy/dimensionaledibles/util/TeleporterHandlerTest.java
@@ -16,12 +16,13 @@ public class TeleporterHandlerTest
         Bootstrap.register();
     }
 
+    final IBlockState bedrock    = Blocks.BEDROCK.getDefaultState();
+    final IBlockState netherrack = Blocks.NETHERRACK.getDefaultState();
+    final IBlockState lava       = Blocks.LAVA.getDefaultState();
+    final IBlockState air        = Blocks.AIR.getDefaultState();
+
     final MockWorld solidBlockWorld = new MockWorld(null, null, new MockHellWorldProvider(), null, false)
     {
-        final IBlockState bedrock = Blocks.BEDROCK.getDefaultState();
-        final IBlockState netherrack = Blocks.NETHERRACK.getDefaultState();
-        final IBlockState air = Blocks.AIR.getDefaultState();
-
         /*
             ...
             Y>=129 Air
@@ -51,11 +52,57 @@ public class TeleporterHandlerTest
         }
     };
 
+    final MockWorld lavaWorld = new MockWorld(null, null, new MockHellWorldProvider(), null, false)
+    {
+        /*
+            ...
+            Y>=129 Air
+            Y=128 Bedrock
+            Y=127 Air
+            Y=126 Air
+            Y=125 Netherrack
+            Y=124 Lava
+            ...
+            Y=5 Lava
+            Y=4 Bedrock
+            ...
+            Y=0 Bedrock
+            Y<=-1 Air
+            ...
+         */
+        @Override
+        public IBlockState getBlockState(BlockPos pos)
+        {
+            if(pos.getY() > 128)
+                return air;
+            else if(pos.getY() == 128)
+                return bedrock;
+            else if(pos.getY() > 125)
+                return air;
+            else if(pos.getY() == 125)
+                return netherrack;
+            else if(pos.getY() <= 4)
+                return bedrock;
+            else if(pos.getY() < 0)
+                return air;
+            else
+                return lava;
+        }
+    };
+
     @Test
     public void getValidYSpawnPos_spawns_players_above_the_bottom_of_the_map()
     {
         BlockPos result = TeleporterHandler.getValidYSpawnPos(solidBlockWorld, BlockPos.ORIGIN);
 
         assertThat("Specified Y places player under the map", result.getY(), greaterThan(4));
+    }
+
+    @Test
+    public void getValidYSpawnPos_will_not_spawn_players_in_lava()
+    {
+        BlockPos result = TeleporterHandler.getValidYSpawnPos(lavaWorld, BlockPos.ORIGIN);
+
+        assertThat("Specified Y places player in lava", result.getY(), greaterThan(124));
     }
 }
