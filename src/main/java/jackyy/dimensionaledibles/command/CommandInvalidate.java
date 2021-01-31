0feef6baf67b7221bc9db3ab1534efe65fd6a97c
ALongStringOfNumbers <sjensen793@gmail.com>
2021-01-31 03:29:32 -0700

    Add command to invalidate spawn locations in a dimension

---
+++ src/main/java/jackyy/dimensionaledibles/command/CommandInvalidate.java
@@ -0,0 +1,50 @@
+package jackyy.dimensionaledibles.command;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.server.command.CommandTreeBase;
+
+import static jackyy.dimensionaledibles.util.TeleporterHandler.getModNBTData;
+
+public class CommandInvalidate extends CommandBase {
+
+    @Override
+    public String getName() {
+        return "invalidate";
+    }
+
+    @Override
+    public String getUsage(ICommandSender sender) {
+        return "dimensionaledibles.command.invalidate.usage";
+    }
+
+    @Override
+    public int getRequiredPermissionLevel() {
+        return 1;
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+
+        try {
+            EntityPlayerMP player = getCommandSenderAsPlayer(sender);
+            NBTTagCompound dimensionCache = getModNBTData(player);
+            if(dimensionCache.hasKey(args[0])) {
+                dimensionCache.removeTag(args[0]);
+                sender.sendMessage(new TextComponentTranslation("dimensionaledibles.command.invalidate.success").setStyle(new Style().setColor(TextFormatting.GREEN)));
+            }
+        }
+        catch (IndexOutOfBoundsException exception) {
+            sender.sendMessage(new TextComponentTranslation("dimensionaledibles.command.invalidate.failed").setStyle(new Style().setColor(TextFormatting.RED)));
+        }
+
+
+    }
+}
